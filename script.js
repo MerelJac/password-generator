95af5ccff033d1c782b2016e24573faef73996ae
Merel Jacobs <merel.burleigh@gmail.com>
2023-05-16 12:04:25 -0700

    Creating button functionality and window prompts

--- script.js
+++ script.js
@@ -4,13 +4,28 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+var writePassword = function () {
+    var uppercasePrompt = window.confirm("Include uppercase letters?");
+    if (!uppercasePrompt) {console.log(false)};
+    var lowercasePrompt = window.confirm("Include lowercase letters?");
+    if (!lowercasePrompt) {console.log(false)};
+    var symbolPrompt = window.confirm("Include symbols?");
+    if (!symbolPrompt) {console.log(false)};
+    var lengthPrompt = window.prompt("Length of password. Please keep between 8 and 128 characters.");
+    if (lengthPrompt > 128 || lengthPrompt < 8 ) {console.log(false)};
+
+    
+
+    
+}
+
+generateBtn.addEventListener("click", function() {console.log("Hi")});
 
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector("password");
 
   passwordText.value = password;
 
@@ -18,4 +33,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
